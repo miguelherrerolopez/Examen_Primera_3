7846581857e7a78011442a93b3ece898edb7c00b
miguelherrerolopez <miguelherrerolopez2017@gmail.com>
2018-12-12 20:46:26 +0100

    Initial commit

---
+++ Examen_Primera_3/Assets/BOLA.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BOLA : MonoBehaviour {
+
+    float velocidad = -0.01f;
+    public KeyCode teclaAsignadaA = KeyCode.A;
+    float izquierda = -0.1f;
+    float derecha = 0.1f;
+    public KeyCode teclaAsignadaD = KeyCode.D;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Translate(new Vector3(0, velocidad, 0));
+        if (Input.GetKeyDown(teclaAsignadaA))
+        {
+            this.transform.Translate(new Vector3(izquierda, 0, 0));
+        }
+        else if (Input.GetKeyDown(teclaAsignadaD))
+        {
+            this.transform.Translate(new Vector3(derecha, 0, 0));
+        }
+    }
+}
